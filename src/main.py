851f1a157a90bb3cbe8a739a454ba7b7200c7d21
hampi songe <1246638505@qq.com>
2026-03-20 17:29:29 +0800

    4

--- src/main.py
+++ src/main.py
@@ -17,7 +17,6 @@ logger = get_logger()
 ex = Experiment("pymarl")
 ex.logger = logger
 ex.captured_out_filter = apply_backspaces_and_linefeeds
-
 results_path = os.path.join(dirname(dirname(abspath(__file__))), "results")
 
 @ex.main
